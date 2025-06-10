10990c04674ca2a2c252d5b61c4379b046a8ab3d
Mertcan <64431136+mertcanyksel@users.noreply.github.com>
2025-06-10 16:54:29 +1000

    Create Data Types int float double bool str etc.

---
+++ dataTypes.cpp
@@ -0,0 +1,87 @@
+// projectTwo.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+/*
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string userName;
+    int userAge;
+    cout << "Hello World!\n";
+    cout << "What is your name ? ";
+    cin >> userName;
+    cout << "What is your age ? ";
+    cin >> userAge;
+    cout << "You`re " << userName << "\t" << userAge << endl;
+
+}
+
+*/
+
+/*
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float studentMark = 100.6f;
+    cout << "You`re result is " << studentMark << endl; 
+    cout << studentMark;
+
+}
+
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 , num2, result;
+    cout << "First number ? " ;
+    cin >> num1;
+    cout << "Second number ? ";
+    cin >> num2;
+    result = num1 + num2;
+    cout << "Result " << result;
+    
+    
+}
+
+
+/*
+#include <iostream>
+using namespace std;
+
+int addNumbers(int x, int y) {
+    return x + y;
+}
+
+int main() {
+    cout << addNumbers(3 , 5);
+    return 0;
+
+}
+
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    string userName1, userName2 , allNames;
+    cout << "What`s the first name ";
+    cin >> userName1;
+    cout << "What`s the second name ";
+    cin >> userName2;
+    allNames = userName1 + userName2;
+    cout << allNames;
+    
+}
+*/
