210ce0bf384d314981c80cd1b876ea8a799e0144
Mertcan <64431136+mertcanyksel@users.noreply.github.com>
2025-06-21 23:27:39 +1000

    Update assesmentOne.cpp

--- assesmentOne.cpp
+++ assesmentOne.cpp
@@ -1,3 +1,4 @@
+UNION DATA TYPE
 #include <iostream>
 using namespace std;
 
@@ -29,3 +30,48 @@ int main() {
 	cout << "Second Value of y :" << mydata.y << endl;
 
 }
+
+
+
+STRUCT DATA TYPE
+struct firstPoint 
+{
+	int x, y;
+};
+
+
+int main() {
+	int input1;
+	cout << "Give first value of array :" ;
+	cin >> input1;
+
+	int input2;
+	cout << "Give second value of array :";
+	cin >> input2;
+
+
+	firstPoint inputs{ input1,input2 };  //'inputs' is a variable  and we initialized with input1 and input2
+	 //Create a pointer and remember pointer is just a variable that hold the memory address
+	//If we use integer pointers , double pointers ,float pointers in the past, now we'll just gonna use
+	//  'firstPoint' pointers
+	
+	firstPoint* p = &inputs;
+	// firstPoint is data type
+	//p is our name of the pointer
+	// so now we can access 'inputs' through 'p'
+
+
+
+	cout << "\n\nAccess via 'inputs' variable\n";
+	cout <<"This is the first value of array " << inputs.x << "\n";
+	cout << "This is the second value of array " << inputs.y << "\n";
+
+
+	cout << "\nAccess via 'p' (pointer)\n";
+	// . has precedence over *
+	cout << "This is the first value of array " << (*p).x << "\n\n";
+
+
+	cout << "With pointer arrows ->\n" ;
+	cout << p->x << "\n";  // equivelent to (*p).x
+	cout << p->y << "\n";
